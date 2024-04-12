a6edf539a340592fd7006af5ead760ad03d7f91b
Stratos Doukakis <stratosver2@gmail.com>
2024-04-12 14:22:23 +0100

    Rework UsersList.js component.

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,31 +1,57 @@
 import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {fetchUsers} from "../store";
+import {useSelector} from "react-redux";
+import {fetchUsers, addUser} from "../store";
+import Button from "./Button";
+import Skeleton from "./Skeleton";
+import {useThunk} from "../hooks/use-thunk";
 
 function UsersList() {
 
-    const dispatch = useDispatch();
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+    const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
 
-    const {isLoading, data, error} = useSelector((state) => {
+    const {data} = useSelector((state) => {
         return state.users;
     });
 
     useEffect(() => {
-        dispatch(fetchUsers())
+        doFetchUsers();
     }, []);
 
-    if (isLoading) {
-        return <div>Loading...</div>;
+    const handleUserAdd = () => {
+        doCreateUser();
+    };
+
+    let content;
+    if (isLoadingUsers) {
+        content = <Skeleton times={6} className={"h-10 w-full"} />;
     }
-    if (error) {
-        return <div>Error fetching data...</div>
+    else if (loadingUsersError) {
+        content = <div>Error fetching data...</div>
+    }
+    else {
+        content = data.map((user) => {
+            return (
+                <div key={user.id} className="mb-2 border rounded">
+                    <div className="flex p-2 justify-between items-center, cursor-pointer">
+                        {user.name}
+                    </div>
+                </div>
+            );
+        });
     }
 
     return (
         <div>
-            {data.length}
+            <div className="flex flex-row justify-between m-3 items-center">
+                <h1 className="m-2 text-xl"> Users</h1>
+                <Button loading={isCreatingUser} onClick={handleUserAdd}>+ Add User</Button>
+                {creatingUserError && 'Error creating user...'}
+            </div>
+            {content}
         </div>
     );
+
 }
 
 export default UsersList;
